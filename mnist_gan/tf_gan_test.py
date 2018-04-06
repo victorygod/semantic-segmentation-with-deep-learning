d96c58b404bafe6ad8afbd4ede14da4be0d359a8
victorygod <htli0719@gmail.com>
2018-04-06 15:45:20 -0400

    debug

--- mnist_gan/tf_gan_test.py
+++ mnist_gan/tf_gan_test.py
@@ -1,6 +1,6 @@
 import tensorflow as tf
 import numpy as np
-import os
+import os, time
 from scipy import misc
 
 os.environ['TF_CPP_MIN_LOG_LEVEL']='2'
@@ -8,70 +8,93 @@ os.environ['TF_CPP_MIN_LOG_LEVEL']='2'
 activation = tf.nn.leaky_relu
 
 
-def discriminator(data_in, reuse = False):
+def discriminator(data_in, reuse = False, batch_norm = True):
 	with tf.variable_scope('discriminator', reuse=reuse):
-		conv1_1 = tf.layers.conv2d(data_in, name = "conv1_1", filters = 16, kernel_size = 5, padding = "same", activation = activation)
-		conv1_1 = tf.layers.batch_normalization(conv1_1)
-		conv1_2 = tf.layers.conv2d(conv1_1, name = "conv1_2", filters = 32, kernel_size = 5, padding = "same", activation = activation)
-		conv1_2 = tf.layers.batch_normalization(conv1_2)
-		conv1_3 = tf.layers.conv2d(conv1_2, name = "conv1_3", strides = (2,2), filters = 32, kernel_size = 5, padding = "same", activation = activation)
-		conv1_3 = tf.layers.batch_normalization(conv1_3)
+		conv1 = tf.layers.conv2d(data_in, name = "conv1", filters = 128, kernel_size = 5, strides = (2,2), padding = "same", activation = activation)
+		conv1 = tf.layers.batch_normalization(conv1, training = True) if batch_norm else conv1
+		conv2 = tf.layers.conv2d(conv1, name = "conv2", filters = 256, kernel_size = 5, strides = (2,2), padding = "same", activation = activation)
+		conv2 = tf.layers.batch_normalization(conv2, training = True) if batch_norm else conv2
+		conv3 = tf.layers.conv2d(conv2, name = "conv3", filters = 512, kernel_size = 5, strides = (2,2), padding = "same", activation = activation)
+		conv3 = tf.layers.batch_normalization(conv3, training = True) if batch_norm else conv3
+		conv4 = tf.layers.conv2d(conv3, name = "conv4", filters = 1024, kernel_size = 5, strides = (2,2), padding = "same", activation = activation)
+		conv4 = tf.layers.batch_normalization(conv4, training = True) if batch_norm else conv4
 
-		conv2_1 = tf.layers.conv2d(conv1_3, name = "conv2_1", filters = 64, kernel_size = 5, padding = "same", activation = activation)
-		conv2_1 = tf.layers.batch_normalization(conv2_1)
-		conv2_2 = tf.layers.conv2d(conv2_1, name = "conv2_2", filters = 64, kernel_size = 5, padding = "same", activation = activation)
-		conv2_2 = tf.layers.batch_normalization(conv2_2)
-		conv2_3 = tf.layers.conv2d(conv2_2, name = "conv2_3", strides = (2,2), filters = 64, kernel_size = 5, padding = "same", activation = activation)
-		conv2_3 = tf.layers.batch_normalization(conv2_3)
+		conv5 = tf.layers.conv2d(conv4, 1, [2, 2],name = "output", strides=(1, 1), padding='valid')
+		output = tf.reshape(conv5, (-1, 1))
 
-		flatten = tf.reshape(conv2_3, (-1, 49*64))
-		fc1 = tf.layers.dense(flatten, 100, name = "fc1", activation = activation)
-		fc1 = tf.layers.batch_normalization(fc1)
-		fc2 = tf.layers.dense(fc1, 50, name = "fc2", activation = activation)
-		fc2 = tf.layers.batch_normalization(fc2)
-
-		output = tf.layers.dense(fc2, 1, name = "output")
-		#output = tf.layers.batch_normalization(output)
-		output = tf.nn.sigmoid(output)
+		# output = tf.layers.batch_normalization(output, training = True)
+		# output = tf.nn.sigmoid(output)
 
 		return output
 
-def generator(data_in, reuse = False):
+def generator(data_in, reuse = False, batch_norm = True):
 	with tf.variable_scope('generator', reuse=reuse):
-		fc1 = tf.layers.dense(data_in, 1000, name = "fc1", activation = activation)
-		fc1 = tf.layers.batch_normalization(fc1)
-		fc2 = tf.layers.dense(fc1, 7*7*64, name = "fc2", activation = activation)
-		fc2 = tf.layers.batch_normalization(fc2)
+		fc1 = tf.layers.dense(data_in, 1024, name = "fc1", activation = activation)
+		fc1 = tf.layers.batch_normalization(fc1, training = True) if batch_norm else fc1
+		fc2 = tf.layers.dense(fc1, 2*2*64, name = "fc2", activation = activation)
+		fc2 = tf.layers.batch_normalization(fc2, training = True) if batch_norm else fc2
 		
-		unflatten = tf.reshape(fc2, (-1, 7, 7, 64))
+		unflatten = tf.reshape(fc2, (-1, 2, 2, 64))
 
-		upsample1 = tf.image.resize_images(unflatten, (14, 14), method = tf.image.ResizeMethod.BILINEAR)
-		conv1_1 = tf.layers.conv2d(upsample1, name = "conv1_1", filters = 64, kernel_size = 5, padding = "same", activation = activation)
-		conv1_1 = tf.layers.batch_normalization(conv1_1)
-		conv1_2 = tf.layers.conv2d(conv1_1, name = "conv1_2", filters = 64, kernel_size = 5, padding = "same", activation = activation)
-		conv1_2 = tf.layers.batch_normalization(conv1_2)
+		upsample1 = tf.image.resize_images(unflatten, (4, 4), method = tf.image.ResizeMethod.BILINEAR)
+		conv1_1 = tf.layers.conv2d(upsample1, name = "conv1_1", filters = 128, kernel_size = 5, padding = "same", activation = activation)
+		conv1_1 = tf.layers.batch_normalization(conv1_1, training = True) if batch_norm else conv1_1
+		conv1_2 = tf.layers.conv2d(conv1_1, name = "conv1_2", filters = 128, kernel_size = 5, padding = "same", activation = activation)
+		conv1_2 = tf.layers.batch_normalization(conv1_2, training = True) if batch_norm else conv1_2
 
-		upsample2 = tf.image.resize_images(conv1_2, (28, 28), method = tf.image.ResizeMethod.BILINEAR)
-		conv2_1 = tf.layers.conv2d(upsample2, name = "conv2_1", filters = 64, kernel_size = 5, padding = "same", activation = activation)
-		conv2_1 = tf.layers.batch_normalization(conv2_1)
-		conv2_2 = tf.layers.conv2d(conv2_1, name = "conv2_2", filters = 64, kernel_size = 5, padding = "same", activation = activation)
-		conv2_2 = tf.layers.batch_normalization(conv2_2)
+		upsample2 = tf.image.resize_images(conv1_2, (7, 7), method = tf.image.ResizeMethod.BILINEAR)
+		conv2_1 = tf.layers.conv2d(upsample2, name = "conv2_1", filters = 128, kernel_size = 5, padding = "same", activation = activation)
+		conv2_1 = tf.layers.batch_normalization(conv2_1, training = True) if batch_norm else conv2_1
+		conv2_2 = tf.layers.conv2d(conv2_1, name = "conv2_2", filters = 128, kernel_size = 5, padding = "same", activation = activation)
+		conv2_2 = tf.layers.batch_normalization(conv2_2, training = True) if batch_norm else conv2_2
 
-		conv3 = tf.layers.conv2d(conv2_2, name = "conv3", filters = 1, kernel_size = 5, padding = "same")
-		conv3 = tf.layers.batch_normalization(conv3)
+		upsample3 = tf.image.resize_images(conv2_2, (14, 14), method = tf.image.ResizeMethod.BILINEAR)
+		conv3_1 = tf.layers.conv2d(upsample3, name = "conv3_1", filters = 128, kernel_size = 5, padding = "same", activation = activation)
+		conv3_1 = tf.layers.batch_normalization(conv3_1, training = True) if batch_norm else conv3_1
+		conv3_2 = tf.layers.conv2d(conv3_1, name = "conv3_2", filters = 128, kernel_size = 5, padding = "same", activation = activation)
+		conv3_2 = tf.layers.batch_normalization(conv3_2, training = True) if batch_norm else conv3_2
 
-		output = tf.nn.relu6(conv3) / 6.0
+
+		upsample4 = tf.image.resize_images(conv3_2, (28, 28), method = tf.image.ResizeMethod.BILINEAR)
+		conv4_1 = tf.layers.conv2d(upsample4, name = "conv4_1", filters = 128, kernel_size = 7, padding = "same", activation = activation)
+		conv4_1 = tf.layers.batch_normalization(conv4_1, training = True) if batch_norm else conv4_1
+		conv4_2 = tf.layers.conv2d(conv4_1, name = "conv4_2", filters = 128, kernel_size = 7, padding = "same", activation = activation)
+		conv4_2 = tf.layers.batch_normalization(conv4_2, training = True) if batch_norm else conv4_2
+
+		conv5 = tf.layers.conv2d(conv4_2, name = "conv5", filters = 1, kernel_size = 7, padding = "same")
+		# conv5 = tf.layers.batch_normalization(conv5, training = True) if batch_norm else conv5
+
+		output = tf.nn.tanh(conv5)
+
+		# conv1 = tf.layers.conv2d_transpose(tf.reshape(data_in, (-1, 1, 1, 100)), 1024, [4, 4], strides=(1, 1), padding='valid')
+		# lrelu1 = activation(tf.layers.batch_normalization(conv1, training=True), 0.2)
+
+		# # 2nd hidden layer
+		# conv2 = tf.layers.conv2d_transpose(lrelu1, 512, [4, 4], strides=(2, 2), padding='same')
+		# lrelu2 = activation(tf.layers.batch_normalization(conv2, training=True), 0.2)
+
+		# # 3rd hidden layer
+		# conv3 = tf.layers.conv2d(lrelu2, 256, kernel_size = (2,2), strides = (1,1), padding = "valid", activation = activation)
+		# lrelu3 = tf.layers.batch_normalization(conv3, training = True)
+
+		# # 4th hidden layer
+		# conv4 = tf.layers.conv2d_transpose(lrelu3, 128, [4, 4], strides=(2, 2), padding='same')
+		# lrelu4 = activation(tf.layers.batch_normalization(conv4, training=True), 0.2)
+
+		# # output layer
+		# conv5 = tf.layers.conv2d_transpose(lrelu4, 1, [4, 4], strides=(2, 2), padding='same')
+		# output = tf.nn.tanh(conv5)
 
 		return output
 
 
 img_in = tf.placeholder("float", shape = (None, 28, 28, 1))
 z_in = tf.placeholder("float", shape = (None, 100))
-label = tf.placeholder("float", shape = (None, 10))
+#label = tf.placeholder("float", shape = (None, 10))
 
 G_z = generator(z_in)
-D_real_logits = discriminator(img_in / 255.0)
-D_fake_logits = discriminator(G_z, reuse=True)
+D_real_logits = discriminator((img_in - 0.5 ) / 0.5, batch_norm = False)
+D_fake_logits = discriminator(G_z, reuse=True, batch_norm = False)
 
 
 batch_size = 50
@@ -86,8 +109,13 @@ D_vars = [var for var in T_vars if var.name.startswith('discriminator')]
 G_vars = [var for var in T_vars if var.name.startswith('generator')]
 
 with tf.control_dependencies(tf.get_collection(tf.GraphKeys.UPDATE_OPS)):
-    D_optim = tf.train.AdamOptimizer(0.0002).minimize(D_loss, var_list=D_vars)
-    G_optim = tf.train.AdamOptimizer(0.0002).minimize(G_loss, var_list=G_vars)
+    D_optim = tf.train.AdamOptimizer(0.0002, beta1 = 0.5).minimize(D_loss, var_list=D_vars)
+    G_optim = tf.train.AdamOptimizer(0.0002, beta1 = 0.5).minimize(G_loss, var_list=G_vars)
+
+s1 = tf.summary.scalar("loss_G", G_loss)
+s2 = tf.summary.scalar("loss_D", D_loss)
+s3 = tf.summary.image("ans", G_z)
+merged_summary = tf.summary.merge_all()
 
 restore_path = 'saver_test/'
 saver = tf.train.Saver(max_to_keep = 1)
@@ -95,6 +123,7 @@ saver = tf.train.Saver(max_to_keep = 1)
 from tensorflow.examples.tutorials.mnist import input_data
 mnist = input_data.read_data_sets("MNIST_data/", one_hot=True)
 
+#np.random.seed(int(time.time()))
 with tf.Session() as sess:
 	init = tf.global_variables_initializer()
 	sess.run(init)
@@ -103,9 +132,10 @@ with tf.Session() as sess:
 	if checkpoint:
 		print("restore from: " + checkpoint)
 		saver.restore(sess, checkpoint)
-
 	
-	for i in range(10000):
+	writer = tf.summary.FileWriter("gan_test/", sess.graph)
+	
+	for i in range(100000):
 		train_data, label_data = mnist.train.next_batch(batch_size)
 		train_data = np.reshape(train_data, (batch_size, 28,28,1))
 		
@@ -115,9 +145,12 @@ with tf.Session() as sess:
 		feed_dict = {img_in: train_data, z_in: np.random.normal(0, 1, (batch_size, 100))}
 		loss_g_, _, out_img = sess.run([G_loss, G_optim, G_z], feed_dict = feed_dict)
 
-		if (i%10==0):
+		if (i%5==0):
 			print(loss_d_)
 			print(loss_g_)
 			print("==============")
 			misc.imsave("ans.jpg", np.reshape(out_img[0], (28,28)))
+
+			s = sess.run(merged_summary, feed_dict = {img_in: train_data, z_in: np.random.normal(0, 1, (batch_size, 100))})
+			writer.add_summary(s, i)
 			saver.save(sess, restore_path, global_step = i)
